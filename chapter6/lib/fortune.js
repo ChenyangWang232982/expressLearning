460931b85803fdcba6497535242eadd74d56ea4a
Chenyang Wang <chenyang.wang@tuni.fi>
2026-01-19 21:36:14 +0200

    Initiate chapter6

---
+++ chapter6/lib/fortune.js
@@ -0,0 +1,15 @@
+const fortunes = [
+ "Conquer your fears or they will conquer you.",
+ "Rivers need springs.",
+ "Do not fear what you don't know.",
+ "You will have a pleasant surprise.",
+ "Whenever possible, keep it simple.",
+];
+exports.getFortune = () => {
+    const fortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    return fortune;
+}
+/**
+ *  If you want something to be visible outside of the
+ *  module, you have to add it to exports
+ */
---
+++ chapter6/meadowlark.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressHandlebars = require('express-handlebars');
+const handlers = require('./lib/handler');
+const app = express();
+const port = process.env.PORT || 3000
+const fortune = require('./lib/fortune')
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars.engine({
+    defaultLayout: 'main',  //the content in views would replace {{{body}}} in main
+}))
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname+'/public'));  //set the files in public forlder can be accessed directly, in that way, public would be invisible to client side
+/**
+ * middleware is processed in order, and static middleware—which is
+ * usually declared first or at least very early—will override other routes.
+ */
+//order: middleeware -> route -> 4../5.. middleware server -> server
+
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
+//middleware
+app.use(handlers.notFound)
+
+app.use(handlers.serverError)
+
+if(require.main === module) {  //Determine if the current file is the entry point file directly executed by Node.js.
+    app.listen(port, ()=>{
+        console.log(`Express started on http://localhost:${port}` + '; press Ctrl-C to terminate.')
+    })
+} else {
+    module.exports = app
+}
